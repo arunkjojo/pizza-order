c55521cf7f39cc48838d4bdf90dc3e591ad54dc9
Arun Jojo <42526032+arunkjojo@users.noreply.github.com>
2022-06-24 09:34:32 +0530

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,6 +4,8 @@ const handler = (req, res) => {
     if(req.method === 'POST') {
         const { username, password } = req.body;
         if(username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD){
+            console.log(username, process.env.ADMIN_USERNAME);
+            console.log(password, process.env.ADMIN_PASSWORD);
             res.setHeader(
                 "Set-Cookie", 
                 cookie.serialize("token", process.env.TOKEN, {
